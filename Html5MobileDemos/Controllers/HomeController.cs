044bef156ff1bf08f6c9600f61f5439207d1ef43
Paul Burgess <paul.j.burgess@outlook.com>
2015-06-14 15:02:49 +0100

    Added basis of simple puzzle game

--- Html5MobileDemos/Controllers/HomeController.cs
+++ Html5MobileDemos/Controllers/HomeController.cs
@@ -13,18 +13,10 @@ namespace Html5MobileDemos.Controllers
             return View();
         }
 
-        public ActionResult About()
+        public ActionResult PuzzleGame()
         {
-            ViewBag.Message = "Your application description page.";
-
             return View();
         }
 
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
     }
 }
